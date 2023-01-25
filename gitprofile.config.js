5f09aae6e1f1467451c8ddb92a49c803f0b8d1df
arnoux23u <arnouxguillaume54@hotmail.fr>
2023-01-25 13:59:00 +0100

    update for garnx.fr

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,110 +2,90 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
-    sortBy: 'stars', // stars | updated
+    username: 'guillaumearnx', // Your GitHub org/user name. (Required)
+    sortBy: 'updated', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
-      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      projects: [
+        "github-readme-stats",
+        "guillaumarnx",
+        "discord-image-generation",
+        "Potato",
+        "pterodactyl-eggs"
+      ], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
+    linkedin: 'arnouxg',
+    twitter: '',
     facebook: '',
-    instagram: '',
+    instagram: 'guillaumearnx',
     dribbble: '',
     behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
-    stackoverflow: '', // format: userid/username
-    website: 'https://arifszn.com',
+    medium: '',
+    dev: 'guillaumearnx',
+    stackoverflow: '13652668/guillaumearnx', // format: userid/username
+    website: 'https://garnx.fr',
     phone: '',
-    email: 'arifulalamszn@gmail.com',
+    email: 'contact@garnx.fr',
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://cv.garnx.fr', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'PHP',
-    'Laravel',
     'JavaScript',
-    'React.js',
     'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
+    'MySQL / PLSQL',
+    'Java',
+    'Scala',
+    'Dart',
     'Git',
     'Docker',
     'PHPUnit',
     'CSS',
-    'Antd',
-    'Tailwind',
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
+      company: 'LMI Solutions',
+      position: 'Computer Science and Network Junior Engineer',
+      from: 'April 2022',
       to: 'Present',
-      companyLink: 'https://example.com',
+      companyLink: 'https://www.lmisolutions.fr',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'Gendarmerie Nationale',
+      position: 'Réserviste opérationnel (RO1)',
+      from: 'Sept 2022',
+      to: 'Present',
+      companyLink: '',
     },
   ],
-  /* certifications: [
-    {
-      name: 'Lorem ipsum',
-      body: 'Lorem ipsum dolor sit amet',
-      year: 'March 2022',
-      link: 'https://example.com'
-    },
-  ], */
   education: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
+      institution: 'CESI',
+      degree: 'Computer Science and Network Engineer',
+      from: '2022',
+      to: 'Present',
+    },
+    {
+      institution: 'IUT Nancy-Charlemagne',
+      degree: 'Computer Science DUT',
+      from: '2020',
+      to: '2022',
     },
     {
       institution: 'Institution Name',
       degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      from: '2017',
+      to: '2020',
     },
   ],
-
   // To hide the `My Projects` section, keep it empty.
-  externalProjects: [
-    {
-      title: 'Project Name',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
-    },
-    {
-      title: 'Project Name',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
-    },
-  ],
+  externalProjects: [],
   // Display blog posts from your medium or dev account. (Optional)
-  blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 2, // How many posts to display. Max is 10.
-  },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
@@ -115,7 +95,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'winter',
+    defaultTheme: 'haloween',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
